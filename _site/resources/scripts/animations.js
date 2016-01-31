3ce7b9e44588bd22344cabbe44c49c6c0f07b560
Yoshie <yoshiemuranaka@gmail.com>
2016-01-31 10:39:38 -0500

    workarounds for browser history

--- _site/resources/scripts/animations.js
+++ _site/resources/scripts/animations.js
@@ -5,7 +5,7 @@ Grace.Animations = {
 	easePage: function($page) {
 		Grace.el.allPages.removeClass('ease');
 		setTimeout(function(){ //this is so I can see the animation
-			$page.addClass('ease');
+			$page.addClass('ease');			
 		}, 10); 
 	},
 
@@ -25,6 +25,7 @@ Grace.Animations = {
 		init: function(e) {
 		
 			e.preventDefault(); //this is to stop the window scroll
+			e.stopPropagation(); //this is to stop bubbling up
 			
 			if(Modernizr.csstransitions && Modernizr.csstransforms) {
 				if(Grace.el.drawerMenu.hasClass('open')) {
@@ -32,9 +33,14 @@ Grace.Animations = {
 				} else {
 					Grace.Animations.drawer.open();
 				};
-			} else {
-
-				//javascript animate top: 200px
+			} else { //using JavaScript to animate drawer
+				if(Grace.el.drawerMenu.hasClass('open')) {
+					Grace.Animations.drawer.close();
+					Grace.el.drawerMenu.animate({'top': '-150px'}, 350)
+				} else {
+					Grace.Animations.drawer.open();
+					Grace.el.drawerMenu.animate({'top': '50px'}, 350)
+				};
 			};	
 
 			Grace.Animations.drawer.events();
@@ -42,17 +48,19 @@ Grace.Animations = {
 
 		open: function() {
 			Grace.el.drawerMenu.addClass('open');
-			$('.overlay').animate({'opacity': '0.75'}, 450);
+			Grace.el.overlay.animate({'opacity': '0.75'}, 450);
+			$('body').addClass('drawer-open'); //this it to prevent scrolling while drawer is open
 		},
 
 		close: function() {
 			Grace.el.drawerMenu.removeClass('open');
-			$('.overlay').css({'opacity': '0'});
+			Grace.el.overlay.css({'opacity': '0'});
+			$('body').removeClass('drawer-open');
 		},
 
 		events: function() {
-			$('.overlay').one('click', Grace.Animations.drawer.close)
-			$('.main-nav__home').one('click', Grace.Animations.drawer.close)
+			Grace.el.overlay.one('click', Grace.Animations.drawer.close)
+			Grace.el.mainNav.one('click', Grace.Animations.drawer.close)
 			Grace.el.links.one('click', Grace.Animations.drawer.close)
 		}
 	}
--- resources/scripts/pagination.js
+++ resources/scripts/pagination.js
@@ -50,8 +50,9 @@ Grace.Pagination = {
 	},
 
 	events: function() {
-		Grace.el.links.click(function(event){
-			event.preventDefault();
+		Grace.el.links.click(function(e){
+			e.preventDefault();
+			e.stopPropagation(); //this is to stop bubbling up
 			Grace.Pagination.pushState($(this));
 		});
 		Grace.el.burgerMenu.click(Grace.Animations.drawer.init)
@@ -71,14 +72,18 @@ $(function(){
 			links: $('.js-page-target'),
 			footer: $('.footer'),
 			activePage: $('.js-animate.active'),
-			burgerMenu: $('.menu--burger'),
-			drawerMenu: $('.js-animate.drawer')
+			burgerMenu: $('.burger-icon'),
+			drawerMenu: $('.js-animate.drawer'), 
+			overlay: $('.drawer .overlay'),
+			mainNav: $('.main-nav')		
 	};
 
 	if (Modernizr.history) {
 		Grace.Pagination.init();
 	} else {
-		//Grace.Animations.easePage();
+		Grace.Animations.easePage($('.page.active'));
+		Grace.el.burgerMenu.click(Grace.Animations.drawer.open)
 	};
+
 
 });
